aa6dfa67e67f3b8fadea26d14fd5be52ba6aa684
AWTMODS <aadithaadith14@gmail.com>
2025-12-06 21:41:46 +0530

    Fixed url not opening issue

--- lib/utils/url_launcher.dart
+++ lib/utils/url_launcher.dart
@@ -2,8 +2,14 @@ import 'dart:developer';
 
 import 'package:url_launcher/url_launcher.dart';
 
-Future<void> launch_Url(_url) async {
-  if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
+Future<void> launch_Url(var _url) async {
+  Uri uri;
+  if (_url is Uri) {
+    uri = _url;
+  } else {
+    uri = Uri.parse(_url.toString());
+  }
+  if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
     log('Could not launch $_url', name: "launch_Url");
   }
 }
